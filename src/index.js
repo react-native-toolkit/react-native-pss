670a2f7d9a1cdce3e3c86263a38f1420cd732ebf
DaniAkash <s.daniakash@gmail.com>
2017-01-22 12:58:23 +0530

    remove async function

--- src/index.js
+++ src/index.js
@@ -4,11 +4,11 @@ import {
 } from 'react-native';
 
 export default {
-  async create(styles) {
+  create(styles) {
     const keys = Object.keys(styles);
     const newStyles = {};
 
-    await keys.forEach(key => {
+    keys.forEach(key => {
       const {ios, android, ...style} = styles[key];
       newStyles[key] = {...style, ...Platform.select({ios, android})};
     });
