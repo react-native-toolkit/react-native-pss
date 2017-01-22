bb3971b7685edcd5c35900ce5785faadf653dc0e
DaniAkash <s.daniakash@gmail.com>
2017-01-22 12:58:01 +0530

    add editorconfig

--- src/index.js
+++ src/index.js
@@ -2,17 +2,17 @@ import {
   Platform,
   StyleSheet,
 } from 'react-native';
- 
+
 export default {
   async create(styles) {
     const keys = Object.keys(styles);
     const newStyles = {};
- 
+
     await keys.forEach(key => {
       const {ios, android, ...style} = styles[key];
       newStyles[key] = {...style, ...Platform.select({ios, android})};
     });
- 
+
     return StyleSheet.create(newStyles);
   },
 };
